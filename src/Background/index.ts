316ee9d53c622256d667d1502a42e69b66bc02fe
William Chong <williamchong007@msn.com>
2020-02-01 03:10:51 +0800

    ✨ Update extension icon and title on tab switch

--- src/Background/index.ts
+++ src/Background/index.ts
@@ -11,6 +11,32 @@ let isLoggedIn = false;
 
 let loginTabId: number | undefined = 0;
 
+
+async function getCurrentTabURL() {
+  const [currentTab] = await browser.tabs.query({ active: true, lastFocusedWindow: true, currentWindow: true});
+  const currentURL = currentTab && currentTab.url;
+  if (!currentURL) {
+    console.error('no url');
+    console.error(currentTab);
+    return '';
+  }
+  return currentURL;
+}
+
+async function updateBookmarkIcon(url?: string) {
+  let currentURL = url;
+  if (!url) {
+    currentURL = await getCurrentTabURL();
+  }
+  if (currentURL && isBookMarked(currentURL)) {
+    browser.browserAction.setIcon({ path: '/assets/icons/bookmark-48.png' });
+    browser.browserAction.setTitle({ title: 'Unbookmark'});
+  } else {
+    browser.browserAction.setIcon({ path: '/assets/icons/bookmark_border-48.png' });
+    browser.browserAction.setTitle({ title: 'Bookmark' });
+  }
+}
+
 async function checkLoginStatus() {
   isLoggedIn = false
   const { data: user } = await api.getLoginStatus();
@@ -18,30 +44,19 @@ async function checkLoginStatus() {
     isLoggedIn = true;
     await refreshBookmark();
     await updateBookmarkIcon();
+  } else {
+    browser.browserAction.setIcon({ path: '/assets/icons/favicon-48.png' });
+    browser.browserAction.setTitle({ title: 'Login to liker.land'});
   }
 }
 
 function handleLikerLandLogin(tabId: number, changeInfo: any) {
+  console.log(changeInfo);
   if (loginTabId === tabId && changeInfo && changeInfo.url) {
     isLoggedIn = changeInfo.url.includes('liker.land/following');
     if (isLoggedIn) {
       browser.tabs.remove(tabId)
     }
-  }
-}
-
-async function handleBookMark() {
-  const [currentTab] = await browser.tabs.query({ active: true, lastFocusedWindow: true, currentWindow: true});
-  const currentURL = currentTab && currentTab.url;
-  if (!currentURL) {
-    console.error('no url');
-    console.error(currentTab);
-    return;
-  }
-  if (isBookMarked(currentURL)) {
-    await removeBookMark(currentURL);
-  } else {
-    await addBookMark(currentURL);
   }
 }
 
@@ -53,18 +68,34 @@ async function doLoginViaLikerLand() {
   }
 }
 
+async function toggleBookMark() {
+  const currentURL = await getCurrentTabURL();
+  if (!currentURL) return;
+  if (isBookMarked(currentURL)) {
+    await removeBookMark(currentURL);
+  } else {
+    await addBookMark(currentURL);
+  }
+  updateBookmarkIcon(currentURL);
+}
+
 browser.runtime.onInstalled.addListener(async (): Promise<void> => {
   await checkLoginStatus();
 });
-
 browser.runtime.onStartup.addListener(async (): Promise<void>  => {
   await checkLoginStatus();
+});
+
+browser.tabs.onActivated.addListener(async (activeInfo) => {
+    const currentTab = await browser.tabs.get(activeInfo.tabId);
+    const currentURL = currentTab && currentTab.url;
+    updateBookmarkIcon(currentURL)
 });
 
 browser.browserAction.onClicked.addListener(async () => {
   if (!isLoggedIn) {
     await doLoginViaLikerLand();
   } else {
-    await handleBookMark();
+    await toggleBookMark();
   }
 });
