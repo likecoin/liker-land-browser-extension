fb781b7097b2b596e23f935945d68811763ace45
William Chong <me@williamchong.cloud>
2021-03-25 18:52:01 +0800

    🌐 Fix like.co register url locale handling

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,22 +1,25 @@
 const translation = {
   en: {
     translation: {
+      LIKE_CO_LOCALE_KEY: 'en',
       YOUTUBE_TITLE: 'Let YouTubers get rewarded by likes!',
-      YOUTUBE_STEP1: 'Register a Liker ID: https://like.co/in/register?language=en',
+      YOUTUBE_STEP1: 'Register a Liker ID: https://like.co/in/register',
       YOUTUBE_STEP2: 'Add the line "https://button.like.co/yourLikerID" at the end of the video description',
     },
   },
   'zh-CN': {
     translation: {
+      LIKE_CO_LOCALE_KEY: 'zh',
       YOUTUBE_TITLE: '按赞让 YouTuber 获得回报！',
-      YOUTUBE_STEP1: '注册 Liker ID： https://like.co/in/register?language=zh',
+      YOUTUBE_STEP1: '注册 Liker ID： https://like.co/in/register',
       YOUTUBE_STEP2: '在影片描述最后加上 https://button.like.co/yourLikerID',
     },
   },
   'zh-TW': {
     translation: {
+      LIKE_CO_LOCALE_KEY: 'zh',
       YOUTUBE_TITLE: '按讚讓 YouTuber 獲得回報！',
-      YOUTUBE_STEP1: '註冊 Liker ID： https://like.co/in/register?language=zh',
+      YOUTUBE_STEP1: '註冊 Liker ID： https://like.co/in/register',
       YOUTUBE_STEP2: '在影片描述最後加上 https://button.like.co/yourLikerID',
     },
   },
