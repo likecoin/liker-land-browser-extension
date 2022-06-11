e5a2b129873c809d20b6c5466c6a0c9922a0b15a
William Chong <6198816+williamchong@users.noreply.github.com>
2022-06-11 23:50:36 +0800

    🚑️ Fix api error would cause infinite loop (#32)
    * 🚑️ Fix api error would cause infinite loop
    
    * 🔥 Simplify injection logic
    
    * 🔥 Remove unused recursion code

--- src/ContentScript/index.ts
+++ src/ContentScript/index.ts
@@ -5,87 +5,31 @@ import { browser } from 'webextension-polyfill-ts';
 import contentConnector from './event-center/content-connector';
 
 class PageInjector {
-  recursiveQueue: number[] = [];
-
-  recursiveNumber = 0;
-
   constructor() {
     contentConnector.init();
-    this.recursiveQueue = [];
-    this.recursiveNumber = 0;
-
     window.addEventListener('message', event => {
       // eslint-disable-next-line no-restricted-globals
       if (event.data.nid === 'pageLoad') {
-        this.injectInpage().then(() => {
-          this.observer();
-        });
+        setTimeout(() => {
+          this.injectInpage();
+        }, 3000);
       }
     });
   }
 
-  callback = (mutationsList: MutationRecord[]) => {
-    // Use traditional 'for loops' for IE 11
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        // @ts-ignore
-        const timeOut: number = setTimeout(() => {
-          this.injectInpage();
-        }, 1000);
-        this.recursiveQueue.push(timeOut);
-      }
-    }
-  };
-
-  observer() {
-    if (document.querySelector('.like-button-container')) {
-      const ele = document.querySelector('#meta-contents') as HTMLElement;
-      // @ts-ignore
-      if (ele?.children.length < 2) {
-        const timeOut = setTimeout(() => {
-          this.injectInpage();
-        }, 1000);
-        // @ts-ignore
-        this.recursiveQueue.push(timeOut);
-      }
-      const observer = new MutationObserver(this.callback);
-
-      const config = { attributes: true, childList: true, subtree: true };
-
-      observer.observe(ele, config);
-    } else {
-      if (this.recursiveNumber > 20) {
-        this.clearRecursiveQueue();
-        return;
-      }
-      const timeOut = setTimeout(() => {
-        this.recursiveNumber += 1;
-        this.observer();
-      }, 1000);
-      // @ts-ignore
-      this.recursiveQueue.push(timeOut);
-    }
-  }
-
-  clearRecursiveQueue() {
-    this.recursiveQueue.forEach(item => {
-      clearTimeout(item);
-    });
-  }
-
-  injectInpage() {
-    return new Promise<boolean>(resolve => {
+  async injectInpage() {
+    const res = await new Promise<boolean>(resolve => {
       // eslint-disable-next-line no-restricted-globals
       if (location.hostname === 'www.youtube.com') {
         const inpage = document.createElement('script');
         inpage.src = browser.runtime.getURL('js/inpage.bundle.js');
-        document.body.appendChild(inpage);
+        document.head.appendChild(inpage);
         inpage.onload = () => {
           resolve(true);
         };
       }
     });
-    // eslint-disable-next-line no-restricted-globals
+    return res;
   }
 }
 
